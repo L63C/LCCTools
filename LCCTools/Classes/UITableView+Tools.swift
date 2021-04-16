b4b6cc5f62341fa8179602cc56280b17ba6247e2
lu63chuan <lu63chuan@163.com>
2021-04-16 17:39:42 +0800

    0.1.1

---
+++ LCCTools/Classes/UITableView+Tools.swift
@@ -0,0 +1,26 @@
+//
+//  UITableView+Tools.swift
+//  LCCTools
+//
+//  Created by L63 on 2021/4/13.
+//
+
+import Foundation
+import UIKit
+
+@objc extension UITableView{
+    public func lc_getoffsetX() -> CGFloat {
+        guard let navController = self.navController else {
+            return 0
+        }
+        let isTranslucent = navController.navigationBar.isTranslucent
+        if isTranslucent {
+            return -navController.navigationBar.frame.maxY
+        }else{
+            return 0
+        }
+    }
+    public func lc_scrollToTop(animated:Bool) {
+        self.setContentOffset(CGPoint.init(x: 0, y: lc_getoffsetX()), animated: animated)
+    }
+}
--- LCCTools/Classes/UIView+Tools.swift
+++ LCCTools/Classes/UIView+Tools.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+/// 延时相关
 extension UIView{
   @objc public func addSubviewsRandomColors() {
         self.backgroundColor = randomColor()
@@ -19,5 +20,48 @@ extension UIView{
     }
     @objc func randomColor() -> UIColor {
         UIColor.init(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1);
+    }
+    
+}
+/// 控制器相关
+extension UIView {
+    //获取view所在的 视图控制器
+    var viewController: UIViewController? {
+        get {
+            var nextResponder = next
+            while  (nextResponder != nil){
+                if nextResponder is UIViewController {
+                    return nextResponder as! UIViewController?
+                }
+                nextResponder = nextResponder?.next
+            }
+            return nil
+        }
+    }
+    //获取view所在的 导航控制器
+    var navController: UINavigationController? {
+        get {
+            var nextResponder = next
+            while  (nextResponder != nil){
+                if nextResponder is UINavigationController {
+                    return nextResponder as! UINavigationController?
+                }
+                nextResponder = nextResponder?.next
+            }
+            return nil
+        }
+    }
+    //获取view所在的 标签控制器
+    var tabBarController: UITabBarController? {
+        get {
+            var nextResponder = next
+            while  (nextResponder != nil){
+                if nextResponder is UITabBarController {
+                    return nextResponder as! UITabBarController?
+                }
+                nextResponder = nextResponder?.next
+            }
+            return nil
+        }
     }
 }
